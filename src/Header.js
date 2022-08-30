83d293f88eb62a4eb7b82ce5408a7faeb72fcb33
Cristhian Pardo <cristhianp@agatadata.com>
2022-08-30 15:21:26 -0500

    Retocando componente Header

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,15 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import HelpIcon from '@mui/icons-material/Help';
 import IconButton from '@mui/material/IconButton';
-import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 import Toolbar from '@mui/material/Toolbar';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
-
-const lightColor = 'rgba(255, 255, 255, 0.7)';
 
 function Header(props) {
     const { onDrawerToggle } = props;
